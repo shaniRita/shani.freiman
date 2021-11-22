89c5d534162152f2630019c3a5033fda63decdf3
shaniRita <shanifreiman@gmail.com>
2021-11-22 23:51:49 +0200

    push

---
+++ Animation.js
@@ -0,0 +1,12 @@
+function myName(){
+    document.getElementById("button").innerHTML = "SHANI RITA FREIMAN";
+    console.log("check");
+}
+
+var x=5, y=7, check= (x==y);
+
+console.log(check)
+
+function changepic(){
+    document.getElementById("Sugar").src ="sugar.jpeg";
+}

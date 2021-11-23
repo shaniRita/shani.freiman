2d12bc919990ef577a134dc399bb48859a7d4830
shaniRita <shanifreiman@gmail.com>
2021-11-23 22:49:01 +0200

    EXC5

--- Animation.js
+++ Animation.js
@@ -3,11 +3,4 @@ function myName(){
     console.log("check");
 }
 
-var x=5, y=7, check= (x==y);
-
-
 console.log(check)
-
-function changepic(){
-    document.getElementById("Sugar").src ="sugar.jpeg";
-}
